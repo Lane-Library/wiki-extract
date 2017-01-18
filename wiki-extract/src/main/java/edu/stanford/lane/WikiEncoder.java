ed0491f32e72dd4e12e119bdb1dac544b8a25e7e
Ryan Max Steinberg <ryanmax@stanford.edu>
2017-01-18 11:28:11 -0800

    initial import

---
+++ wiki-extract/src/main/java/edu/stanford/lane/WikiEncoder.java
@@ -0,0 +1,60 @@
+package edu.stanford.lane;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ryanmax
+ */
+public class WikiEncoder {
+
+    private int fieldToEncode;
+
+    private String inputFile;
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /*
+     * used this to URL encode DOIs from CrossRef (Joe Wass)
+     */
+    public WikiEncoder(final String inputFile, final int fieldToEncode) {
+        this.inputFile = inputFile;
+        this.fieldToEncode = fieldToEncode;
+        File in = new File(this.inputFile);
+        if (in.exists()) {
+            extract(in);
+        }
+    }
+
+    public static void main(final String[] args) {
+        new WikiEncoder(args[0], Integer.parseInt(args[1]));
+    }
+
+    private void extract(final File input) {
+        try (BufferedReader br = new BufferedReader(new FileReader(input));
+                FileWriter fw = new FileWriter(this.inputFile + "-encoded.txt", false);) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split("\t");
+                fields[this.fieldToEncode] = URLDecoder.decode(fields[this.fieldToEncode], "UTF-8");
+                StringBuilder sb = new StringBuilder();
+                for (String field : fields) {
+                    sb.append(field);
+                    sb.append("\t");
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                sb.append("\n");
+                fw.write(sb.toString());
+            }
+        } catch (IOException e) {
+            this.log.error(e.getMessage(), e);
+        }
+    }
+}
