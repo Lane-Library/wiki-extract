851b751f4858846cce1dc14e93141b83f3b38cad
Ryan Max Steinberg <ryanmax@stanford.edu>
2017-01-18 17:18:39 -0800

    lowercase DOIs (to match Joe Wass CrossRef data), resolve shortened DOIs

--- wiki-extract/src/main/java/edu/stanford/lane/DOIParser.java
+++ wiki-extract/src/main/java/edu/stanford/lane/DOIParser.java
@@ -1,6 +1,10 @@
 package edu.stanford.lane;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLDecoder;
 
 public final class DOIParser {
@@ -13,22 +17,21 @@ public final class DOIParser {
      * @return DOI or empty if no DOI found
      */
     public static String parse(final String link) {
-        String s = link;
-        if (null != s) {
+        String parsed = link;
+        if (null != parsed) {
             try {
-                s = URLDecoder.decode(s, "UTF-8");
+                parsed = URLDecoder.decode(parsed, "UTF-8");
             } catch (UnsupportedEncodingException e) {
                 // won't happen
             }
         }
-        s = s.replaceFirst(".*\\.org/", "");
-        s = s.trim();
-        // http://www.doi.org/doi_handbook/2_Numbering.html#2.6.3
-        if (s.startsWith("doi:")) {
-            s = s.substring(4);
+        parsed = removePrefixes(parsed);
+        if (isShortDoi(parsed)) {
+            parsed = resolveShortenedDoi(parsed);
         }
-        if (s.startsWith("10.") || isShortDoi(s)) {
-            return s;
+        parsed = parsed.toLowerCase().trim();
+        if (parsed.startsWith("10.")) {
+            return parsed;
         }
         return "";
     }
@@ -41,5 +44,31 @@ public final class DOIParser {
      */
     private static boolean isShortDoi(final String doi) {
         return doi.matches("\\w+") && !doi.matches("\\d+");
+    }
+
+    private static String removePrefixes(final String doi) {
+        String s = doi;
+        s = s.replaceFirst("(?i).*\\.org/", "");
+        // http://www.doi.org/doi_handbook/2_Numbering.html#2.6.3
+        s = s.replaceFirst("(?i)^doi:", "");
+        return s;
+    }
+
+    private static String resolveShortenedDoi(final String doi) {
+        URL url = null;
+        URLConnection connection = null;
+        String resolvedDoi = removePrefixes(doi);
+        try {
+            url = new URL("http://doi.org/" + doi);
+            connection = url.openConnection();
+            ((HttpURLConnection) connection).setInstanceFollowRedirects(false);
+        } catch (IOException e) {
+            throw new WikiExtractException("can't fetch doi: " + url, e);
+        }
+        String location = connection.getHeaderField("Location");
+        if (null != location) {
+            resolvedDoi = location;
+        }
+        return removePrefixes(resolvedDoi);
     }
 }
--- wiki-extract/src/test/java/edu/stanford/lane/DOIParserTest.java
+++ wiki-extract/src/test/java/edu/stanford/lane/DOIParserTest.java
@@ -14,16 +14,20 @@ public class DOIParserTest {
                 DOIParser.parse("http://dx..doi.org/10.1016/j.biocon.2012.09.011"));
         assertEquals("10.1001/archderm.1965.01600170101020",
                 DOIParser.parse("//dx.doi.org/10.1001%2Farchderm.1965.01600170101020"));
-        assertEquals("10.1002/(SICI)(1997)5:2<86::AID-NT6>3.0.CO;2-7",
+        assertEquals("10.1002/(sici)(1997)5:2<86::aid-nt6>3.0.co;2-7",
                 DOIParser.parse("//dx.doi.org/10.1002%2F(SICI)(1997)5:2%3C86::AID-NT6%3E3.0.CO;2-7"));
-        assertEquals("10.1002/(SICI)1096-8644(199703)102:3\\%3C301::AID-AJPA1\\%3E3.0.CO;2-Y", DOIParser
+        // source DOI is double encoded ... would never resolve
+        assertEquals("10.1002/(sici)1096-8644(199703)102:3\\%3c301::aid-ajpa1\\%3e3.0.co;2-y", DOIParser
                 .parse("//dx.doi.org/10.1002%2F(SICI)1096-8644(199703)102:3%5C%253C301::AID-AJPA1%5C%253E3.0.CO;2-Y"));
         assertEquals("10.1002/pros.1131 [pii]", DOIParser.parse("//dx.doi.org/10.1002%2Fpros.1131+%5Bpii%5D"));
         assertEquals("", DOIParser.parse("http://www.doi.org/news/DOINewsApr11.html#1"));
-        assertEquals("3d2", DOIParser.parse("http://doi.org/3d2"));
-        assertEquals("bptps5", DOIParser.parse("http://doi.org/bptps5"));
+        assertEquals("10.1177/0021943612436973", DOIParser.parse("http://doi.org/3d2"));
+        assertEquals("10.5465/amj.2009.43669971", DOIParser.parse("http://doi.org/bptps5"));
         assertEquals("", DOIParser.parse("http://doi.org/10"));
         assertEquals("", DOIParser.parse("http://doi.org/012345678"));
-        assertEquals("10.1016/S0007-0785(96)80056-1", DOIParser.parse("http://doi.org/doi:10.1016/S0007-0785(96)80056-1"));
+        assertEquals("10.1016/s0007-0785(96)80056-1", DOIParser.parse("http://doi.org/doi:10.1016/S0007-0785(96)80056-1"));
+        assertEquals("10.1016/s0007-0785(96)80056-1", DOIParser.parse("http://doi.org/DOI:10.1016/S0007-0785(96)80056-1"));
+        assertEquals("10.1016/s0007-0785(96)80056-1", DOIParser.parse("http://DOI.ORG/doi:10.1016/S0007-0785(96)80056-1"));
+        assertEquals("10.1002/047084289x.rd071", DOIParser.parse("10.1002/047084289x.rd071"));
     }
 }
