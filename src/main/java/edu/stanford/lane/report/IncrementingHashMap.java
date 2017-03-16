6671c989db4be571a6747eb0ec55e68c9b9d4c62
Ryan Max Steinberg <ryanmax@stanford.edu>
2017-03-16 13:40:43 -0700

    simplify

--- src/main/java/edu/stanford/lane/report/IncrementingHashMap.java
+++ src/main/java/edu/stanford/lane/report/IncrementingHashMap.java
@@ -6,32 +6,12 @@ public class IncrementingHashMap extends HashMap<Object, Integer> {
 
     private static final long serialVersionUID = 1L;
 
-    private HashMap<Object, Integer> hashMap;
-
-    public IncrementingHashMap() {
-        this.hashMap = new HashMap<>();
-    }
-
     public void add(final Object key) {
         int count = 1;
-        if (this.hashMap.containsKey(key)) {
-            count = this.hashMap.get(key).intValue() + 1;
+        if (super.containsKey(key)) {
+            count = super.get(key).intValue() + 1;
         }
-        this.hashMap.put(key, new Integer(count));
+        super.put(key, Integer.valueOf(count));
     }
 
-    @Override
-    public boolean containsKey(final Object key) {
-        return this.hashMap.containsKey(key);
-    }
-
-    @Override
-    public Integer get(final Object key) {
-        return this.hashMap.get(key);
-    }
-
-    @Override
-    public Integer put(final Object key, final Integer value) {
-        return this.hashMap.put(key, value);
-    }
 }
